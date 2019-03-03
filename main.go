6676ee8338363e4fcecc12045e0f4b2f5b30f590
Anton Martynov <ntn@ntn-Inspiron-15-7000-Gaming.(none)>
2019-03-03 18:59:09 +0300

    CreateUser func complete;)

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+// ToDo бахнуть обработку ошибок
 
 type User struct {
 	ID         int
@@ -22,20 +24,33 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user User
 	_ = json.NewDecoder(r.Body).Decode(&user)
+
 	users = append(users, user)
 	json.NewEncoder(w).Encode(user)
 }
 
 func editUser(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, item := range users {
+		id, _ := strconv.Atoi(params["ID"])
+		if item.ID == id {
+			var user User
+			_ = json.NewDecoder(r.Body).Decode(&user)
+			user.ID = id
+			u := &users[i]
+			*u = user
+			return
+		}
+	}
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for _, item := range users {
-		fmt.Println(users)
-		if item.Nickname == params["Nickname"] {
+		id, _ := strconv.Atoi(params["ID"])
+		if item.ID == id {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
@@ -46,6 +61,7 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 func main() {
 	reciever := mux.NewRouter()
 	reciever.HandleFunc("/createUser", createUser).Methods("POST")
-	reciever.HandleFunc("/users/{Nickname}", getUser).Methods("GET")
+	reciever.HandleFunc("/users/{ID}", getUser).Methods("GET")
+	reciever.HandleFunc("/settings/{ID}", editUser).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", reciever))
 }
