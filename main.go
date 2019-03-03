dd0133a210368153204b2f8f35c07fd37a23928d
evv <evv.february@gmail.com>
2019-03-03 18:27:26 +0300

    # Create and get methods( store in memory )

--- main.go
+++ main.go
@@ -2,8 +2,50 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
-func main() {
+type User struct {
+	ID         int
+	Nickname   string
+	password   string
+	CardNumber string
+	CashPoints int
+}
 
+var users []User
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+}
+
+func editUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range users {
+		fmt.Println(users)
+		if item.Nickname == params["Nickname"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&User{})
+}
+
+func main() {
+	reciever := mux.NewRouter()
+	reciever.HandleFunc("/createUser", createUser).Methods("POST")
+	reciever.HandleFunc("/users/{Nickname}", getUser).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", reciever))
 }
