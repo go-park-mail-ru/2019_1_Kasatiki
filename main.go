153113651f237d61eafeed04f5127f5f28a39221
evv <evv.february@gmail.com>
2019-03-06 01:02:31 +0300

    # Modify create user ( generate uid, add check to exists )

--- main.go
+++ main.go
@@ -8,57 +8,74 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"os/exec"
 )
 
 // ToDo бахнуть обработку ошибок
 
 type User struct {
-	ID         int
-	Nickname   string
-	password   string
-	CardNumber string
-	CashPoints int
+	ID       string
+	Nickname string `json:"nickname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Points   int
 }
 
 var users []User
 
+func (u *User) setUniqueId() {
+	// DB incremental or smth
+	out, _ := exec.Command("uuidgen").Output()
+	u.ID = string(out[:len(out)-1])
+}
+
 func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var user User
-	_ = json.NewDecoder(r.Body).Decode(&user)
-	users = append(users, user)
-	json.NewEncoder(w).Encode(user)
-}
-
-func editUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for i, item := range users {
-		id, _ := strconv.Atoi(params["ID"])
-		if item.ID == id {
-			var user User
-			_ = json.NewDecoder(r.Body).Decode(&user)
-			user.ID = id
-			u := &users[i]
-			*u = user
+	var newUser User
+	error := map[string]string{
+		"Error": "Nickname already exists",
+	}
+	_ = json.NewDecoder(r.Body).Decode(&newUser) // ToDo: Log error
+	for _, existUser := range users {
+		if newUser.Nickname == existUser.Nickname || newUser.Email == existUser.Email {
+			json.NewEncoder(w).Encode(error)
 			return
 		}
 	}
+	newUser.setUniqueId()
+	users = append(users, newUser) // Check succesfull append? ( in db clearly )
+	json.NewEncoder(w).Encode(newUser)
+
 }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, item := range users {
-		id, _ := strconv.Atoi(params["ID"])
-		if item.ID == id {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&User{})
-}
+//func editUser(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("Content-Type", "application/json")
+//	params := mux.Vars(r)
+//	for i, item := range users {
+//		id, _ := strconv.Atoi(params["ID"])
+//		if item.ID == id {
+//			var user User
+//			_ = json.NewDecoder(r.Body).Decode(&user)
+//			user.ID = id
+//			u := &users[i]
+//			*u = user
+//			return
+//		}
+//	}
+//}
+
+//func getUser(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("Content-Type", "application/json")
+//	params := mux.Vars(r)
+//	for _, item := range users {
+//		id, _ := strconv.Atoi(params["ID"])
+//		if item.ID == id {
+//			json.NewEncoder(w).Encode(item)
+//			return
+//		}
+//	}
+//	json.NewEncoder(w).Encode(&User{})
+//}
 
 func upload(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(32 << 20)
@@ -68,7 +85,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer file.Close()
-	f, err := os.OpenFile("./public/img/"+"test1.jpg", os.O_WRONLY|os.O_CREATE, 0666) // ToDo: Change way to handle img
+	f, err := os.OpenFile("./static/img/"+"test1.jpg", os.O_WRONLY|os.O_CREATE, 0666) // ToDo: Change way to handle img
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -80,9 +97,9 @@ func upload(w http.ResponseWriter, r *http.Request) {
 func main() {
 	reciever := mux.NewRouter()
 	reciever.HandleFunc("/signup", createUser).Methods("POST")
-	reciever.HandleFunc("/users/{ID}", getUser).Methods("GET")
-	reciever.HandleFunc("/settings/{ID}", editUser).Methods("POST")
+	//reciever.HandleFunc("/users/{ID}", getUser).Methods("GET")
+	//reciever.HandleFunc("/settings/{ID}", editUser).Methods("POST")
 	reciever.HandleFunc("/upload", upload).Methods("POST")
-	reciever.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
+	reciever.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	log.Fatal(http.ListenAndServe(":8080", reciever))
 }
