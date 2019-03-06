ab57e136afec7e65ba6c27f9260f53170b75ea72
evv <evv.february@gmail.com>
2019-03-07 00:34:30 +0300

    # Remove error declaration from func, add raw auth scheme

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -30,6 +32,15 @@ type Order struct {
 	Sequence string `json:"order"`
 }
 
+//ToDo: Move to another package
+var errorLogin = map[string]string{
+	"Error": "User dont exist",
+}
+
+var errorCreateUser = map[string]string{
+	"Error": "Nickname/mail already exists",
+}
+
 var users []User
 
 func (u *User) setUniqueId() {
@@ -42,9 +53,6 @@ func (u *User) setUniqueId() {
 func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var newUser User
-	errorCreateUser := map[string]string{
-		"Error": "Nickname/mail already exists",
-	}
 	_ = json.NewDecoder(r.Body).Decode(&newUser) // ToDo: Log error
 	for _, existUser := range users {
 		if newUser.Nickname == existUser.Nickname || newUser.Email == existUser.Email {
@@ -71,21 +79,70 @@ func getLeaderboard(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-//func editUser(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "application/json")
-//	params := mux.Vars(r)
-//	for i, item := range users {
-//		id, _ := strconv.Atoi(params["ID"])
-//		if item.ID == id {
-//			var user User
-//			_ = json.NewDecoder(r.Body).Decode(&user)
-//			user.ID = id
-//			u := &users[i]
-//			*u = user
-//			return
-//		}
-//	}
-//}
+func createSessionId(user User) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"nickname": user.Nickname,
+		"email":    user.Email,
+		"about":    user.About,
+		"region":   user.Region,
+		"img":      user.ImgUrl,
+		"age":      user.Age,
+	})
+	// ToDo: Error handle
+	spiceSalt, _ := ioutil.ReadFile("secret.conf")
+	secretStr, _ := token.SignedString(spiceSalt)
+	return secretStr
+}
+
+func checkAuth(cookie *http.Cookie) jwt.MapClaims {
+	token, _ := jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		spiceSalt, _ := ioutil.ReadFile("secret.conf")
+		return spiceSalt, nil
+	})
+
+	claims, _ := token.Claims.(jwt.MapClaims)
+	// ToDo: Handle else case
+	return claims
+}
+
+func editUser(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session_id")
+	if err != nil {
+		w.Write([]byte("=("))
+		return
+	}
+	claims := checkAuth(cookie)
+	w.Write([]byte(claims["email"].(string)))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var sessionId string
+	var userExistFlag bool
+	var existUser User
+	userExistFlag = false
+	_ = json.NewDecoder(r.Body).Decode(&existUser)
+	for _, user := range users {
+		if user.Nickname == existUser.Nickname && user.Password == existUser.Password {
+			userExistFlag = true
+			existUser = user
+		}
+	}
+	if !userExistFlag {
+		json.NewEncoder(w).Encode(errorLogin)
+		return
+	}
+	sessionId = createSessionId(existUser)
+	fmt.Println(sessionId)
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: sessionId,
+	}
+	http.SetCookie(w, cookie)
+	json.NewEncoder(w).Encode(existUser)
+}
 
 // ToDO: Add case sensitive ( high/low )
 func getUser(w http.ResponseWriter, r *http.Request) {
@@ -121,7 +178,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Mocked part for leaderboard
 	var mockedUser = User{"1", "evv", "onetaker@gmail.com",
-		"nechitai", -100, 23, "test",
+		"evv", -100, 23, "test",
 		"Voronezh", "В левой руке салам"}
 	var mockedUser1 = User{"2", "tony", "trendpusher@hydra.com",
 		"qwerty", 100, 22, "test",
@@ -133,10 +190,11 @@ func main() {
 	// GET  ( get exists data )
 	reciever.HandleFunc("/users/{Nickname}", getUser).Methods("GET")
 	reciever.HandleFunc("/leaderboard", getLeaderboard).Methods("GET")
+	reciever.HandleFunc("/edit", editUser).Methods("GET")
 	// POST ( create new data )
 	reciever.HandleFunc("/signup", createUser).Methods("POST")
 	reciever.HandleFunc("/upload", upload).Methods("POST")
-	//reciever.HandleFunc("/settings/{ID}", editUser).Methods("POST")
+	reciever.HandleFunc("/login", login).Methods("POST")
 	reciever.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	log.Fatal(http.ListenAndServe(":8080", reciever))
 }
