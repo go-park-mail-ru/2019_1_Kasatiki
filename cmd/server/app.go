c7077fa6c5b6dc95a698e640ae3ec67a0bc41d83
evv <evv.february@gmail.com>
2019-04-17 01:03:33 +0300

    # Structure refactoring

--- cmd/server/app.go
+++ cmd/server/app.go
@@ -1,4 +1,4 @@
-package app
+package server
 
 import (
 	"fmt"
@@ -15,7 +15,7 @@ import (
 import (
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
-	"github.com/go-park-mail-ru/2019_1_Kasatiki/internal/pkg/models"
+	"github.com/go-park-mail-ru/2019_1_Kasatiki/pkg/models"
 	"log"
 )
 
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,5 @@
 package server
 
-import (
-	"github.com/go-park-mail-ru/2019_1_Kasatiki/internal/pkg/app"
-)
-
 func main() {
 	advhater := App{}
 	advhater.Initialize()
