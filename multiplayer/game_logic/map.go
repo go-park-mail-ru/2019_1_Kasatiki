e35bc11ee17ac45e296ce24693cc203af2c2321c
Gel0bmstu <leshikne@gmail.com>
2019-05-08 01:22:05 +0300

    ws added

--- multiplayer/game_logic/map.go
+++ multiplayer/game_logic/map.go
@@ -4,11 +4,39 @@ import (
 	"fmt"
 )
 
-type Slice []int
+// Создание карты
+func MapGeneration() Map {
 
-func (instance Slice) BuildMap(blockCount int) {
+	var m Map
+
+	// Инициализируем параметры карты
+	m.TileSize = 10
+	m.SizeX = 100
+	m.SizeY = 100
+
+	blockCount := 4
+
+	// Логика заполнения карты препятствиями:
+	// Делим карту на 16 блоков (4x4 каждый по 25 тайлов)
+	// Заполняем каждый блок препятствиями
+
+	// blockX := ( m.SizeX  ) / blockCount;
+	// blockY := ( m.SizeY ) / blockCount;
+
+	// Создаем границы карты
+
+	// Генерируем вертикальные границы
+	// for i := 0; i < m.SizeY; i++ {
+	// 	for j := 0; j < m.SizeX; j++ {
+	// 		if i == 0 || i == m.SizeY-1 || j == 0 || j == m.SizeX -1 {
+    //             m.Field[m.SizeY * i + j] = 1;
+    //         } 
+	// 	}
+	// }
+
 	// Задаем массив шаблонов карт:
 	// Каждый шаблон - массив 20x20, заполенный препядствием
+
 	var templates [][]int
 
 	template1 := []int{
@@ -74,55 +102,23 @@ func (instance Slice) BuildMap(blockCount int) {
 		for j := 0; j < blockCount; j++ {
 			template := templates[rand.Intn(len(templates))]
 			for k := 0; k < len(template); k++ {
-				instance = append(instance, template[k])
+				m.Field = append(m.Field, template[k])
 			}
 		}
 	}
-}
 
-// Создание карты
-func MapGeneration() Map {
-
-	var m Map
-
-	// Инициализируем параметры карты
-	m.TileSize = 10
-	m.SizeX = 100
-	m.SizeY = 100
-
-	blockCount := 4
-
-	// Логика заполнения карты препятствиями:
-	// Делим карту на 16 блоков (4x4 каждый по 25 тайлов)
-	// Заполняем каждый блок препятствиями
-
-	blockX := ( m.SizeX  ) / blockCount;
-	blockY := ( m.SizeY ) / blockCount;
-
-	// Создаем границы карты
-
-	// Генерируем вертикальные границы
-	for i := 0; i < m.SizeY; i++ {
-		for j := 0; j < m.SizeX; j++ {
-			if i == 0 || i == m.SizeY-1 || j == 0 || j == m.SizeX -1 {
-                m.Field[m.SizeY * i + j] = 1;
-            } 
-		}
-	}
-
-	// Итератор, которым мы проходим по саммиву шаблона
-	//iter := 0
-
-	m.Field.BuildMap(blockCount)
 
 	// Отрисовываем результат в консоль
-	for i := 0; i < 10; i++ {
-		for j := 0; i < 10; j++ {
-			// fmt.Println(i, j)
-			fmt.Print(a[i*10+j])
-		}
-		fmt.Print("\n")
-	}
+	// for i := 0; i < 10; i++ {
+	// 	for j := 0; i < 10; j++ {
+	// 		// fmt.Println(i, j)
+	// 		fmt.Print(m.Field[i*10+j])
+	// 	}
+	// 	fmt.Println("")
+	// }
+
+	fmt.Println(m.Field)
+	fmt.Println("succes")
+
 
 	return m
-}
