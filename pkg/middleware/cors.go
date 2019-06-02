f2d0460334b921f1523941a0d4a930accce7f4e1
evvfebruary <evv.bmstu@gmail.com>
2019-06-02 10:49:12 +0300

    # Cors off

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -3,7 +3,7 @@ package middleware
 import "github.com/gin-gonic/gin"
 
 func (instance *Middlewares) CORSMiddleware(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "www.advhater.ru")
+	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 	c.Header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Kasatiki-X-28")
